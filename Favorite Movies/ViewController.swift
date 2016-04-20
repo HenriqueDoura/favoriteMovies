71e7da29374bb7478ff2eca8bce71b2acfb2ef4e
Henrique Dourado <Henrique@mac-de-henrique.local>
2016-04-20 20:31:28 -0300

    Initial Commit

---
+++ Favorite Movies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favorite Movies
+//
+//  Created by Henrique Dourado on 20/04/16.
+//  Copyright © 2016 Henrique Dourado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
