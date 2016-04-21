9178288260f463b095734182c3d8437de64c2300
Henrique Dourado <Henrique@Mac-de-Henrique.local>
2016-04-20 22:31:20 -0300

    All done

--- Favorite Movies/MovieInfoVC.swift
+++ Favorite Movies/MovieInfoVC.swift
@@ -12,12 +12,23 @@ class MovieInfoVC: UIViewController {
     
     @IBOutlet weak var movieImage: UIImageView!
     @IBOutlet weak var movieTitle: UILabel!
-
+    @IBOutlet weak var moviePlot: UILabel!
+    @IBOutlet weak var movieURLBtn: UIButton!
+    
     var movie = FavoriteMovie()
     
     override func viewDidLoad() {
         super.viewDidLoad()
             movieTitle.text = movie.movieTitle
             movieImage.image = UIImage(data: movie.movieImage)
+            moviePlot.text = movie.moviePlot
+        
+            movieURLBtn.layer.cornerRadius = 25
+            movieImage.layer.cornerRadius = 10
+            movieImage.clipsToBounds = true
+    }
+    
+    @IBAction func movieURLBtnPressed(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string: movie.movieUrl)!)
     }
 }
